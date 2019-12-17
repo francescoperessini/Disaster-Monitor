19e8537d07f871e37e71bc0205f4ad4ac7a63423
stefanomartina <stefano.martina@mail.polimi.it>
2019-12-17 10:20:58 +0100

    Basic structure

---
+++ Disaster Monitor/Navigation/AppNavigation.swift
@@ -0,0 +1,9 @@
+//
+//  AppNavigation.swift
+//  Disaster Monitor
+//
+//  Created by Stefano Martina on 17/12/2019.
+//  Copyright © 2019 Stefano Martina. All rights reserved.
+//
+
+import Foundation
---
+++ Disaster Monitor/UI/MainViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewController.swift
+//  Disaster Monitor
+//
+//  Created by Stefano Martina on 17/12/2019.
+//  Copyright © 2019 Stefano Martina. All rights reserved.
+//
+
+import Foundation
