6485c95963c65154f834ed35c3fa7750ad9fc5a5
stefanomartina <stefano.martina@mail.polimi.it>
2019-12-17 09:46:18 +0100

    Initial Commit

---
+++ Disaster Monitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Disaster Monitor
+//
+//  Created by Stefano Martina on 17/12/2019.
+//  Copyright © 2019 Stefano Martina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
